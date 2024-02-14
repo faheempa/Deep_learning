aa786ec8bbc552f1072e099b3558a3a88afbfe81
faheempa <faheempa788@gmail.com>
2024-02-14 14:17:27 +0530

    update

--- Regularization/L2_regularization.py
+++ Regularization/L2_regularization.py
@@ -17,18 +17,21 @@ labels[iris.species == "versicolor"] = 1
 labels[iris.species == "virginica"] = 2
 
 # making train and test sets using sklearn
-train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2)
+train_data, test_data, train_labels, test_labels = train_test_split(
+    data, labels, test_size=0.2
+)
 # convert them into pytorch datasets
 train_dataset = TensorDataset(train_data, train_labels)
 test_dataset = TensorDataset(test_data, test_labels)
 # make dataloaders
 train_loader = DataLoader(train_dataset, batch_size=12, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=True)
-# checking shapes 
+# checking shapes
 # for data, labels in train_loader:
 #     print(f"train data: {data.shape} train labels: {labels.shape}")
 # for data, labels in test_loader:
 #     print(f"test data: {data.shape} test labels: {labels.shape}")
+
 
 def createModel(L2Lambda):
     # ANN and parameters
@@ -42,6 +45,7 @@ def createModel(L2Lambda):
     lossFun = nn.CrossEntropyLoss()  # loss function includes softmax as well
     optimizer = torch.optim.SGD(ANNiris.parameters(), lr=0.01, weight_decay=L2Lambda)
     return ANNiris, lossFun, optimizer
+
 
 def training(ANNiris, lossFun, optimizer, epochs=500):
     # training
@@ -66,7 +70,7 @@ def training(ANNiris, lossFun, optimizer, epochs=500):
 
             # calculate accuracy
             predLabel = torch.argmax(output, axis=1)
-            batchacc.append(torch.mean((predLabel == labels).float())*100)
+            batchacc.append(torch.mean((predLabel == labels).float()) * 100)
 
         # train accuracy
         train_acc[epoch] = np.mean(batchacc)
@@ -75,9 +79,10 @@ def training(ANNiris, lossFun, optimizer, epochs=500):
         ANNiris.eval()
         pred = ANNiris(test_data)
         predLabel = torch.argmax(pred, axis=1)
-        test_acc[epoch] = torch.mean((predLabel == test_labels).float())*100
-        
+        test_acc[epoch] = torch.mean((predLabel == test_labels).float()) * 100
+
     return train_acc, test_acc
+
 
 # create and train model
 L2Lambda = 0.01
@@ -93,10 +98,12 @@ plt.title(f"lambda: {L2Lambda}, final test accuracy: {test_acc[-1]:.2f}")
 plt.legend()
 plt.show()
 
+
 def smooth(x, k=5):
     return np.convolve(x, np.ones(k) / k, mode="same")
 
-# expiriment - varaing lambda from 0 to 0.1 in 10 steps
+
+# expiriment - varaing lambda from 0 to 0.5 in 25 steps
 L2Lambda = np.linspace(0, 0.5, 25)
 epochs = 250
 final_test_acc = []
@@ -122,8 +129,8 @@ plt.show()
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 for i, l2 in enumerate(L2Lambda):
-    if(l2 > 0.15):
-        break;
+    if l2 > 0.15:
+        break
     plt.plot(train_acc_results[:, i], label=f"lambda: {l2:.2f}")
 plt.xlabel("epochs")
 plt.ylabel("train accuracy")
@@ -133,9 +140,9 @@ plt.legend()
 # plot train accuracy vs lambda vs epochs
 plt.subplot(1, 2, 2)
 for i, l2 in enumerate(L2Lambda):
-    if(l2 > 0.15):
-        break;
-    plt.plot(test_acc_results  [:, i], label=f"lambda: {l2:.2f}")
+    if l2 > 0.15:
+        break
+    plt.plot(test_acc_results[:, i], label=f"lambda: {l2:.2f}")
 plt.xlabel("epochs")
 plt.ylabel("test accuracy")
 plt.title("test accuracy vs epochs vs lambda")
@@ -145,13 +152,24 @@ plt.show()
 # average train and test accuracy in 50-150 epochs range
 noOFLambda = np.where(L2Lambda < 0.15)[0][-1]
 epochrange = [50, 150]
-train_acc_avg = np.mean(train_acc_results[epochrange[0]:epochrange[1],:noOFLambda], axis=0)
-test_acc_avg = np.mean(test_acc_results[epochrange[0]:epochrange[1], :noOFLambda], axis=0)
-plt.plot(L2Lambda[:noOFLambda], train_acc_avg, label="train accuracy", color="red", marker="o")
-plt.plot(L2Lambda[:noOFLambda], test_acc_avg, label="test accuracy", color="blue", marker="o")
-plt.xlabel("lambda") 
+train_acc_avg = np.mean(
+    train_acc_results[epochrange[0] : epochrange[1], :noOFLambda], axis=0
+)
+test_acc_avg = np.mean(
+    test_acc_results[epochrange[0] : epochrange[1], :noOFLambda], axis=0
+)
+plt.plot(
+    L2Lambda[:noOFLambda],
+    train_acc_avg,
+    label="train accuracy",
+    color="red",
+    marker="o",
+)
+plt.plot(
+    L2Lambda[:noOFLambda], test_acc_avg, label="test accuracy", color="blue", marker="o"
+)
+plt.xlabel("lambda")
 plt.ylabel("accuracy")
 plt.title("lambda vs accuracy")
 plt.legend()
 plt.show()
-
